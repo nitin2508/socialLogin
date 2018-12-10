e0d1ed54a051e2daba5f128799b7f5f2d053de3c
nitin <25nitin.nagar@gmail.com>
2018-12-11 00:53:13 +0530

    login

--- components/GoogleLogin.js
+++ components/GoogleLogin.js
@@ -1,32 +1,61 @@
 import React,{Component} from 'react';
 import {View,Text,StyleSheet,Button,AsyncStorage} from 'react-native';
-class HomeScreen extends Component{
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
-    signOut = async()=>{
-        AsyncStorage.clear();
-        this.props.navigation.navigate('AuthLoading');
+class GoogleLogin extends Component{
+
+    componentDidMount(){
+        try {
+            GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true }).then(()=>{
+                console.log('services')
+            })
+            GoogleSignin.configure({
+                scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+                webClientId: '600105741744-55vbuipkrvtesklca27o29c62coctnv0.apps.googleusercontent.com',
+              });
+        // google services are available
+        } catch (err) {
+            console.error('play services are not available');
+        }
     }
+    signIn = async () => {
+      try {
+        GoogleSignin.hasPlayServices().then(()=>{
+           GoogleSignin.signIn().then((info)=>{
+            console.log('user info',info);
+           },(err)=>{
+             console.log('error',err)
+           })
+         
+        })
+      } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+          console.log('1')
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (f.e. sign in) is in progress already
+          console.log('2')
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+          console.log('3')
+        } else {
+            console.log('4')
+          // some other error happened
+        }
+      }
+    };
 
     render(){
         return  (
-            <View style={styles.container}>
-               <Button
-                onPress={()=>this.signOut()}
-                title="SignOut"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-                />
-            </View>
+            <GoogleSigninButton
+            title="LearnMore"
+            style={{ width:312, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Light}
+            onPress={this.signIn} 
+          />
         )
     }
 }
 
-export default HomeScreen;
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
+export default GoogleLogin;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,70 +1,24 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,Image,Button} from 'react-native';
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 import HomeCarosel from '../components/login/HomeCarosel';
+import GoogleLogin from '../components/GoogleLogin';
 
 export default class WelcomeScreen extends Component {
-  componentDidMount(){
-    try {
-        GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true }).then(()=>{
-            console.log('services')
-        })
-        GoogleSignin.configure({
-            scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-            webClientId: '600105741744-55vbuipkrvtesklca27o29c62coctnv0.apps.googleusercontent.com',
-          });
-    // google services are available
-    } catch (err) {
-        console.error('play services are not available');
-    }
-}
-signIn = async () => {
-  try {
-    GoogleSignin.hasPlayServices().then(()=>{
-       GoogleSignin.signIn().then((info)=>{
-        console.log('user info',info);
-       },(err)=>{
-         console.log('error',err)
-       })
-     
-    })
-  } catch (error) {
-    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-      // user cancelled the login flow
-    } else if (error.code === statusCodes.IN_PROGRESS) {
-      // operation (f.e. sign in) is in progress already
-    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-      // play services not available or outdated
-    } else {
-      // some other error happened
-    }
-  }
-};
+
 signOut = ()=>{
   this.props.navigation.navigate('Register');
 }
   render() {
     return (
       <View style={styles.container}>
-           <GoogleSigninButton
-        title="LearnMore"
-        style={{ width:312, height: 48 }}
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Light}
-        onPress={this.signIn} 
-      />
-        <Image
+      
+      <Image
         style={{width: 60, height: 60}}
         source={{uri: 'https://images.upwardly.in/logos/upwardly_emblem_green.png'}}
         />
-    <HomeCarosel/>
-        <Button
-          onPress={()=>this.signOut()}
-          title="SignOut"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-          />
+      <HomeCarosel/>
+      <GoogleLogin/>
       </View>
     );
   }
